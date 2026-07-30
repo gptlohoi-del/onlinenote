d4e91592b6cd861b9e4db4aba74b304e0c333859
gptlohoi-del <gptlohoi@gmail.com>
2026-07-30 21:23:39 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'note-app-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Ép service worker mới hoạt động ngay lập tức
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    // Chiến lược Network First: Luôn lấy code mới nhất từ internet
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => {
+        // Nếu mất mạng, mới lấy từ cache ra dùng
+        return caches.match(event.request);
+      })
+  );
+});
